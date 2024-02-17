1de4135a8c489b32b51ca8d0cad73ab7ca0c9612
suraj.pal <surajpal.enjay@gmail.com>
2024-02-17 11:17:23 +0530

    added bottom navigation

---
+++ lib/pages/feed_page.dart
@@ -0,0 +1,21 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class FeedPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+   return _FeedPage();
+  }
+
+
+}
+class _FeedPage extends State<FeedPage>{
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(color: Colors.purple,);
+  }
+
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,18 @@
-
-
-
 import 'package:flutter/material.dart';
+import 'package:instaclone/pages/feed_page.dart';
+import 'package:instaclone/pages/profile_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-   return _HomePageState();
+    return _HomePageState();
   }
-
 }
-class _HomePageState extends State<HomePage>{
+
+class _HomePageState extends State<HomePage> {
+  int _currentPage = 0 ;
+  final List<Widget> _pages =[FeedPage(),ProfilePage()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,19 +20,36 @@ class _HomePageState extends State<HomePage>{
         title: const Text("Finstagram"),
         actions: [
           GestureDetector(
-            onTap: (){},
+            onTap: () {},
             child: const Icon(Icons.add_a_photo),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 35.0,right: 25.0),
+            padding: const EdgeInsets.only(left: 35.0, right: 25.0),
             child: GestureDetector(
-              onTap: (){},
+              onTap: () {},
               child: const Icon(Icons.logout),
             ),
           ),
         ],
       ),
+      bottomNavigationBar: _bottomNavigationBar(),
+      body:_pages[_currentPage] ,
     );
   }
 
+  Widget _bottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: _currentPage,
+      onTap: (_index){
+        setState(() {
+          _currentPage = _index;
+        });
+
+      },
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.feed), label: "Feed"),
+        BottomNavigationBarItem(icon: Icon(Icons.account_box), label: "Profile"),
+      ],
+    );
+  }
 }
